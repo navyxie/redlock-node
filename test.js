0992274fd12b9c139b094907dce1585dbad304f2
navyxie <navyxie2010@gmail.com>
2016-05-12 01:10:49 +0800

    update unit test and update readme file

--- test.js
+++ test.js
@@ -1,27 +1,151 @@
 var should = require('should');
 var RedLock = require('./index');
+var async = require('async');
 describe('redlock',function(){
   this.timeout(5000);
   var client = require('redis').createClient(6380,'localhost');
   var redlock = new RedLock(client);
-  describe('test single',function(){
-    var lock;
-    it('#lock()',function(done){
-      redlock.lock('test-resource-lock',3,function(err,lockObj){
-        lock = lockObj;
-        done(err);
-      }) 
-    });
-    it('#lock()',function(done){
-      redlock.lock('test-resource-lock',3,function(err,data){
-        should.exists(err);
-        done();
-      }) 
-    });
-    it('#unlock()',function(done){
-      redlock.unlock(lock,function(err,data){
-        done(err);
-      })
-    });
+  describe('callback',function(){
+    describe('test single',function(){
+      var lock;
+      it('#lock() ok.',function(done){
+        redlock.lock('test-resource-lock',3,function(err,lockObj){
+          lock = lockObj;
+          done(err);
+        }) 
+      });
+      it('#lock() not ok.',function(done){
+        redlock.lock('test-resource-lock',3,function(err,data){
+          should.exists(err);
+          done();
+        }) 
+      });
+      it('#extend() ok.',function(done){
+        redlock.extend(lock,3,function(err,data){
+          should.not.exists(err);
+          done();
+        })
+      });
+      it('#unlock() ok.',function(done){
+        redlock.unlock(lock,function(err,data){
+          done(err);
+        })
+      });
+      it('#extend() not ok.',function(done){
+        redlock.extend(lock,3,function(err,data){
+          should.exists(err);
+          done();
+        })
+      });
+      it('#unlock() not ok.',function(done){
+        redlock.unlock(lock,function(err,data){
+          should.exists(err);
+          done();
+        })
+      });
+      it('#unlock() not ok, when resource expired.',function(done){
+        var waterfallFnCallCount = 0;
+        async.waterfall([
+          function(cb){
+            waterfallFnCallCount++;
+            redlock.lock('test-resource-lock',1,function(err,lockObj){
+              setTimeout(function(){
+                cb(err,lockObj);
+              },1001);
+            }); 
+          },
+          function(lockObj,cb){
+            waterfallFnCallCount++;
+            redlock.lock('test-resource-lock',1,function(err,lockObj2){
+              redlock.unlock(lockObj2,function(err){
+                cb(err,lockObj);
+              })
+            })
+          },
+          function(lockObj,cb){
+            waterfallFnCallCount++;
+            redlock.unlock(lockObj,function(err,data){
+              cb(err);
+            })
+          }
+        ],function(err){
+          should.exists(err);
+          waterfallFnCallCount.should.be.equal(3);
+          done(null);
+        });
+      });
+    })
+  });
+  describe('promise',function(){
+    describe('test single',function(){
+      var originLock;
+      it('#lock() ok.',function(done){
+        redlock.lock('test-resource-lock',3).done(function(lock){
+          originLock = lock;
+          lock.resource.should.be.equal('test-resource-lock');
+          done();
+        },done) 
+      });
+      it('#lock() not ok.',function(done){
+        redlock.lock('test-resource-lock',3).done(done,function(err){
+          should.exists(err);
+          done();
+        }); 
+      });
+      it('#extend() ok.',function(done){
+        redlock.extend(originLock,3).done(function(lock){
+          lock.should.Object();
+          done();
+        },done);
+      });
+      it('#unlock() ok.',function(done){
+        redlock.unlock(originLock).done(function(lock){
+          lock.should.Object();
+          done();
+        },done)
+      });
+      it('#extend() not ok.',function(done){
+        redlock.extend(originLock,3).done(done,function(err){
+          should.exists(err);
+          done();
+        })
+      });
+      it('#unlock() not ok.',function(done){
+        redlock.unlock(originLock).done(done,function(err){
+          should.exists(err);
+          done();
+        })
+      });
+      it('#unlock() not ok, when resource expired.',function(done){
+        var waterfallFnCallCount = 0;
+        async.waterfall([
+          function(cb){
+            waterfallFnCallCount++;
+            redlock.lock('test-resource-lock',1).done(function(lockObj){
+              setTimeout(function(){
+                cb(null,lockObj);
+              },1001);
+            },cb); 
+          },
+          function(lockObj,cb){
+            waterfallFnCallCount++;
+            redlock.lock('test-resource-lock',1).done(function(lockObj2){
+              redlock.unlock(lockObj2).done(function(){
+                cb(null,lockObj);
+              },cb);
+            },cb);
+          },
+          function(lockObj,cb){
+            waterfallFnCallCount++;
+            redlock.unlock(lockObj).done(cb,cb);
+          }
+        ],function(err){
+          should.exists(err);
+          waterfallFnCallCount.should.be.equal(3);
+          done(null);
+        });
+      });
+
+    })
   })
 })
